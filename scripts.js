e2a38480fa26aa22152c7b28b30e8e18d5b7dee5
Kaplaugher <kaplaugher94@gmail.com>
2016-12-23 12:03:12 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
-    
-  
-     // the base url for all api calls
+$(document).ready(function() {
+
+    // the base url for all api calls
     var apiBaseUrl = 'http://api.themoviedb.org/3/';
     // the base url for searching
     var imageBaseUrl = 'http://image.tmdb.org/t/p/';
@@ -12,30 +11,17 @@ $(document).ready(function(){
 
     const popularMovies = apiBaseUrl + 'movie/popular?api_key=' + apiKey;
 
-    $.getJSON(nowPlayingUrl, function (nowPlayingData) {
+    $.getJSON(nowPlayingUrl, function(nowPlayingData) {
         console.log(nowPlayingData);
         var nowPlayingHTML = '';
         for (let i = 0; i < nowPlayingData.results.length; i++) {
             var poster = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
-            nowPlayingHTML += '<div class="grid">';
-            nowPlayingHTML += '<div class="grid-item">';
+            nowPlayingHTML += '<div class="grid-item latest col-sm-3">';
             nowPlayingHTML += '<img src="' + poster + '">';
-            nowPlayingHTML += '</div>';
             nowPlayingHTML += '</div>';
         }
 
-        $('#movieContainer').html(nowPlayingHTML);
-    });
-    
-    $('.iso-nav ul li').click(function(){
-                $('.iso-nav ul li').removeClass('active');
-                $(this).addClass('active');
+        $('.gridContainer').html(nowPlayingHTML);
+    })
 
-                var selector = $(this).attr('data-filter');
-                console.log(selector);
-                $('.main-iso').isotope({
-                    filter: selector
-                });
-                return false;
-            });
 });
